fd3b2bdd4553d84a6a26ade87209cdeab46ac0a8
1stVyacheslav <komaroff4@gmail.com>
2019-07-02 23:33:50 +0500

    add spinner in person-details.js, add style person-details

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
 
 import './person-details.css';
 
@@ -8,11 +9,19 @@ export default class PersonDetails extends Component {
 	swapi = new SwapiService();
 	
 	state = {
-		person: null
+		person: null,
+		loading: false
+	}
+
+	componentWillUpdate(newProps) {
+		if (newProps !== this.props) {
+			this.setState( {loading: true })
+		}
+		
 	}
 
 	componentDidMount() {
-		this.updatePerson();
+		// this.updatePerson();
 	}
 
 	componentDidUpdate(prevProps) {
@@ -31,45 +40,62 @@ export default class PersonDetails extends Component {
 		this.swapi.getPerson( this.props.personId )
 							.then( (person) => {
 								// console.log(person);
-								this.setState({ person: person }) 
+								this.setState({ 
+									person: person,
+									loading: false
+								 }) 
 							})
 
 	}
 	
 	render() {
-
-		if (!this.state.person) {
-			return <span>Please, select person!</span>
-		}
 		
-		const { id, name, gender, birthYear, eyeColor } = this.state.person;
+		const { person, loading } = this.state,
+					message = (!person && !loading) ? <Message /> : null,
+					spinner = loading ? <Spinner /> : null,
+					content = (!loading && !!person) ? <Details person={ person } /> : null;
 
 		return (
 			
 				<div className='person-details card d-flex flex-row'>
-					<img className='person-image' src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} alt='person'/>
-	
-					<div className='card-body'>
-						<h4 className='card-title'>{name}</h4>
-						<ul className='list-group list-group-flush'>
-							<li className='list-group-item'>
-								<span className='mr-3'>Gender</span>
-								<span>{gender}</span>
-							</li>
-							<li className='list-group-item'>
-								<span className='mr-3'>Birth Year</span>
-								<span>{birthYear}</span>
-							</li>
-							<li className='list-group-item'>
-								<span className='mr-3'>Eye Color</span>
-								<span>{eyeColor}</span>
-							</li>
-						</ul>
-					</div>
-	
-				</div>
-			
+					{message}
+					{spinner}
+					{content}	
+				</div>		
 	
 		)
 	};
+};
+
+const Details = ( {person} ) => {
+
+	const { id, name, gender, birthYear, eyeColor } = person;
+
+	return (
+		<React.Fragment>
+			<img className='person-image' src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} alt='person'/>
+	
+			<div className='card-body'>
+				<h4 className='card-title'>{name}</h4>
+				<ul className='list-group list-group-flush'>
+					<li className='list-group-item'>
+						<span className='mr-3'>Gender</span>
+						<span>{gender}</span>
+					</li>
+					<li className='list-group-item'>
+						<span className='mr-3'>Birth Year</span>
+						<span>{birthYear}</span>
+					</li>
+					<li className='list-group-item'>
+						<span className='mr-3'>Eye Color</span>
+						<span>{eyeColor}</span>
+					</li>
+				</ul>
+			</div>
+		</React.Fragment>
+	)
+};
+
+const Message = () => {
+	return <span className='message-text'>Please, select person!</span>
 }
