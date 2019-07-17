cb338918e3bcd04c54d0dbe3b45bb4136d871d83
1stVyacheslav <komaroff4@gmail.com>
2019-07-17 16:23:22 +0500

    Switch (handling non-existent addresses). (les.103, end of sec.9)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,7 +8,7 @@ import { PersonDetails } from '../sw-components';
 
 import './app.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 export default class App extends Component {
 
 	swapiService = new SwapiService();
@@ -37,28 +37,35 @@ export default class App extends Component {
 						<Header />
 						<RandomPlanet />
 
-						<Route 	path='/' 
-										render={ () => <h2>Welcom to StarDB</h2> } 
-										exact />
-						<Route path='/people' exact component={PeoplePage} />
-						<Route 	path='/people/:id'
-										render={ ( {match} ) => {
-											const { id } = match.params;
-											return <PersonDetails itemId={id} />
-										} } />
+						<Switch> {/* отрисовывает только первый Route, который соответствует адресу */}
 
-						<Route path='/planet' component={PlanetPage} />
-						<Route path='/starship/:id?' component={StarshipPage} />
+							<Route 	path='/' 
+											render={ () => <h2 className='text-center'>Welcom to StarDB</h2> } 
+											exact />
+							<Route path='/people' exact component={PeoplePage} />
+							<Route 	path='/people/:id'
+											render={ ( {match} ) => {
+												const { id } = match.params;
+												return <PersonDetails itemId={id} />
+											} } />
 
-						{/*Login- and Secret- Pages*/}
-						<Route 	path='/secret' 
-										render={ () => <SecretPage isLoggedIn={isLoggedIn}/> }/>
-						<Route	 path='/login' 
-										render={ () => <LoginPage 
-																		isLoggedIn={isLoggedIn} 
-																		onLogin={ this.onLogin } /> 
-													} />
+							<Route path='/planet' component={PlanetPage} />
+							<Route path='/starship/:id?' component={StarshipPage} />
 
+							{/*Login- and Secret- Pages*/}
+							<Route 	path='/secret' 
+											render={ () => <SecretPage isLoggedIn={isLoggedIn}/> }/>
+							<Route	 path='/login' 
+											render={ () => <LoginPage 
+																			isLoggedIn={isLoggedIn} 
+																			onLogin={ this.onLogin } /> 
+														} />
+							
+							{/*{<Redirect to='/' /> } - этот элемент будет отрисовываться, если не было совпадений по предыдущим Route'ам*/}
+
+							<Route render={ () => <h2 className='text-center'>Page not found</h2> } /> {/* Route без атрибута path будет отрисовываться всегда */}
+
+						</Switch>
 
 					</Router>
 					
